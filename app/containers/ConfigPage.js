d000d4794c2da524829701eb2774ffd09eb154c7
zeroleak <e17ba2c64b67f25a1cddb9bd80f4c53a2df7e9b3>
2019-01-17 17:33:05 +0100

    configuration: add setup mode

--- app/containers/ConfigPage.js
+++ app/containers/ConfigPage.js
@@ -1,5 +1,6 @@
 // @flow
 import React, { Component } from 'react';
+import * as Icon from 'react-feather';
 
 type Props = {};
 
@@ -14,7 +15,7 @@ export default class ConfigPage extends Component<Props> {
         <form>
           <div className="form-group row">
             <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Network</label>
-            <div className="col-sm-10">
+            <div className="col-sm-8">
               <select className="form-control" id="exampleFormControlSelect1" disabled>
                 <option>Testnet</option>
               </select>
@@ -22,13 +23,22 @@ export default class ConfigPage extends Component<Props> {
           </div>
           <div className="form-group row">
             <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">TOR</label>
-            <div className="form-check form-check-inline">
-              <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" disabled/>
-              <label className="form-check-label" htmlFor="inlineCheckbox1">Enable TOR (coming soon)</label>
+            <div className="col-sm-8">
+              <div className="form-check form-check-inline">
+                <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" disabled/>
+                <label className="form-check-label" htmlFor="inlineCheckbox1">Enable TOR (coming soon)</label>
+              </div>
             </div>
           </div>
           <div className="form-group row">
-            <div className="col-sm-10">
+            <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Setup mode</label>
+            <label htmlFor="inputPassword3" className="col-sm-8 col-form-label"><Icon.CheckSquare /> Run whirlpool locally</label>
+          </div>
+          <div className="form-group row">
+            <div className="col-sm-5">
+              <button type="submit" className="btn btn-danger btn-xs">Reset configuration</button>
+            </div>
+            <div className="col-sm-5">
               <button type="submit" className="btn btn-primary">Save</button>
             </div>
           </div>
--- app/containers/InitPage.js
+++ app/containers/InitPage.js
@@ -22,33 +22,65 @@ class InitPage extends Component<Props> {
   constructor(props) {
     super(props)
     this.state = {
-      step: 0
+      step: 0,
+      setupMode: 'local'
     }
+    this.handleOptionChange = this.handleOptionChange.bind(this)
+  }
+
+  handleOptionChange(changeEvent) {
+    const value = changeEvent.target.value
+    console.log('handleOptionChange: '+value)
+    this.setState({
+      setupMode: value
+    });
   }
 
   render() {
     const goNextStep = () => this.setState({step: this.state.step+1})
     const goPrevStep = () => this.setState({step: this.state.step-1})
+    const STEP_LAST = 3
+
 
     return (
       <div>
         <h1>First run</h1>
 
-        {this.state.step !== 2 &&
-          <p>Welcome to <b>whirlpool-gui</b> configuration wizard.<br/>
-          Follow these steps to connect to your existing Samourai Wallet.</p>
-        }
-
         {this.state.step === 0 &&
         <form onSubmit={goNextStep}>
+          <p>Welcome to <b>whirlpool-gui</b> configuration wizard.</p>
           <div className="form-group row">
-            <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Seed words</label>
+            <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Setup mode</label>
             <div className="col-sm-10">
-              <input type="password" className="form-control" id="inputEmail3" placeholder="Enter your existing wallet's seed (12 words)" required autoFocus/>
+              <div className="form-check">
+                <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="local" checked={this.state.setupMode === 'local'} onChange={this.handleOptionChange}/>
+                  <label className="form-check-label" htmlFor="exampleRadios1">
+                    Run whirlpool locally
+                  </label>
+              </div>
+              <div className="form-check">
+                <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="remote" checked={this.state.setupMode === 'remote'} onChange={this.handleOptionChange} />
+                  <label className="form-check-label" htmlFor="exampleRadios2">
+                    Connect to your existing DOJO / whirlpool-client-cli<br/>
+                    {this.state.setupMode === 'remote' &&
+                      <div className="row">
+                        <div className="col-sm-7">
+                          <input type="text" className="form-control" id="inputEmail3" placeholder="host" required/>
+                        </div>
+                        <div className="col-sm-3">
+                         <input type="number" className="form-control" id="inputEmail3" placeholder="port" value={8082} required/>
+                        </div>
+                      </div>
+                    }
+                  </label>
+              </div>
             </div>
           </div>
           <div className="form-group row">
             <div className="col-sm-5">
+              {this.state.step > 0 && <a onClick={goPrevStep}>
+                <i className="fa fa-arrow-left fa-3x" />
+              </a>}
             </div>
             <div className="col-sm-5">
               <button type="submit" className="btn"> <i className="fa fa-arrow-right fa-3x" /></button>
@@ -59,6 +91,8 @@ class InitPage extends Component<Props> {
 
         {this.state.step === 1 &&
         <form onSubmit={goNextStep}>
+          <p>This will connect whirlpool to your existing Samourai Wallet.<br/>
+          Your passphrase will be required for each whirlpool startup.</p>
           <div className="form-group row">
             <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Passphrase</label>
             <div className="col-sm-10">
@@ -67,9 +101,9 @@ class InitPage extends Component<Props> {
           </div>
           <div className="form-group row">
             <div className="col-sm-5">
-              <a onClick={goPrevStep}>
+              {this.state.step > 0 && <a onClick={goPrevStep}>
                 <i className="fa fa-arrow-left fa-3x" />
-              </a>
+              </a>}
             </div>
             <div className="col-sm-5">
               <button type="submit" className="btn"> <i className="fa fa-arrow-right fa-3x" /></button>
@@ -79,11 +113,31 @@ class InitPage extends Component<Props> {
         }
 
         {this.state.step === 2 &&
+        <form onSubmit={goNextStep}>
+          <p>This will connect whirlpool to your existing Samourai Wallet.<br/>
+            Your seed will be encrypted in ./whirlpool-client-cli.conf. <b>whirlpool-gui will never ask again for it.</b></p>
+          <div className="form-group row">
+            <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Seed words</label>
+            <div className="col-sm-10">
+              <input type="password" className="form-control" id="inputEmail3" placeholder="Enter your existing wallet's seed (12 words)" required autoFocus/>
+            </div>
+          </div>
+          <div className="form-group row">
+            <div className="col-sm-5">
+              {this.state.step > 0 && <a onClick={goPrevStep}>
+                <i className="fa fa-arrow-left fa-3x" />
+              </a>}
+            </div>
+            <div className="col-sm-5">
+              <button type="submit" className="btn"> <i className="fa fa-arrow-right fa-3x" /></button>
+            </div>
+          </div>
+        </form>
+        }
+
+        {this.state.step === STEP_LAST &&
         <div>
-          <p>Success, <b>whirlpool-gui</b> is now configured.<br/>
-            Your seed has been encrypted in ./whirlpool-gui.conf. Your passphrase will be required for each GUI startup.<br/><br/>
-            <b>This app will never ask for your seed, you should never type it again on this app.</b>
-          </p>
+          <p>Configuration success. <b>whirlpool-gui</b> is now configured.</p>
           <Link to={routes.STATUS}><button type="button" className="btn btn-primary">Start Whirlpool</button></Link>
         </div>
         }
