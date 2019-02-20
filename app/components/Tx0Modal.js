0d42d822ffb7922c7860737043d71c55a2372b78
zeroleak <zeroleak@samourai.io>
2019-02-20 17:54:55 +0100

    add Tx0Modal + async pools fetching + upgrade to bootstrap4

---
+++ app/components/Tx0Modal.js
@@ -0,0 +1,117 @@
+// @flow
+import React, { Component } from 'react';
+import { Button, Modal, Alert } from 'react-bootstrap';
+import '../containers/PremixPage.css';
+import moment from 'moment';
+import walletService from '../services/walletService';
+import utils from '../services/utils';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import backendService from '../services/backendService';
+
+export default class DepositPage extends React.PureComponent {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      pools: undefined,
+      poolId: undefined,
+      mixsTarget: 0,
+      error: undefined,
+      loaded: false
+    };
+
+    // fetch pools for tx0
+    if (props.utxo) {
+      backendService.tx0.fetchPools(props.utxo.value).then(poolsResponse => {
+        console.log('poolsResponse', poolsResponse)
+        const pools = poolsResponse.pools
+
+        let error = undefined
+        if (pools.length == 0) {
+          error = "No pool found for this utxo value."
+        }
+        this.setState({
+          pools: pools,
+          poolId: pools.length > 0 ? pools[0].poolId : undefined,
+          mixsTarget: 0,
+          error: error,
+          loaded: true
+        })
+      })
+    }
+
+    this.handleChangePoolTx0 = this.handleChangePoolTx0.bind(this);
+    this.handleChangeMixsTargetTx0 = this.handleChangeMixsTargetTx0.bind(this);
+    this.handleSubmitTx0 = this.handleSubmitTx0.bind(this)
+
+  }
+
+  handleChangePoolTx0(e) {
+    const poolId = e.target.value
+
+    this.setState({
+      poolId: poolId
+    })
+  }
+
+  handleChangeMixsTargetTx0(e) {
+    const mixsTarget = parseInt(e.target.value)
+
+    this.setState({
+      mixsTarget: mixsTarget
+    })
+  }
+
+  handleSubmitTx0() {
+    backendService.tx0.create(this.props.utxo.hash, this.props.utxo.index, this.state.poolId, this.state.mixsTarget).then((tx0Response) => {
+      console.log('tx0Response',tx0Response)
+      walletService.fetchState()
+    })
+    this.props.onClose();
+  }
+
+  render() {
+    return (
+      <Modal show={true} onHide={this.handleCloseTx0} dialogClassName="modal-tx0">
+        <Modal.Header>
+          <Modal.Title>Add to premix<br/>
+            <small>{this.props.utxo.hash}:{this.props.utxo.index}</small></Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            This will split your UTXO and start mixing.<br/>
+            Value: <strong>{utils.toBtc(this.props.utxo.value)}btc</strong><br/>
+            <br/>
+
+            {this.state.loaded && this.state.error && (
+              <Alert variant='danger'>{this.state.error}</Alert>
+            )}
+            {this.state.loaded && !this.state.error && (
+              <div>
+                Select a pool:
+                <select className="form-control" onChange={this.handleChangePoolTx0} defaultValue={this.state.poolId}>
+                  {this.state.pools.map(pool => <option key={pool.poolId} value={pool.poolId}>{pool.poolId} (denomination: {utils.toBtc(pool.denomination)}btc, anonymity set: {pool.mixAnonymitySet}, connected: {pool.nbRegistered}, last mix: {moment.duration(pool.elapsedTime).humanize()})</option>)}
+                </select><br/>
+
+                Mixs target:
+                <select className="form-control col-sm-2" onChange={this.handleChangeMixsTargetTx0} defaultValue={this.state.mixsTarget}>
+                  <option value={1}>1</option>
+                  <option value={2}>2</option>
+                  <option value={3}>3</option>
+                  <option value={5}>5</option>
+                  <option value={10}>10</option>
+                  <option value={0}>∞</option>
+                </select> (you can change this later)
+              </div>
+            )}
+        </Modal.Body>
+        <Modal.Footer>
+          {!this.state.loaded && <div className="modal-status">
+            <div className="spinner-border spinner-border-sm" role="status"/> Fetching pools for tx0...
+          </div>}
+          <Button variant="secondary" onClick={this.props.onClose}>Close</Button>
+          {this.state.loaded && !this.state.error && <Button onClick={this.handleSubmitTx0}>Start mixing</Button>}
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
--- app/containers/DepositPage.js
+++ app/containers/DepositPage.js
@@ -5,15 +5,14 @@ import QRCode from 'qrcode.react';
 import './PremixPage.css';
 import * as Icon from 'react-feather';
 import { bindActionCreators } from 'redux';
-import moment from 'moment';
 import { walletActions } from '../actions/walletActions';
 import { connect } from 'react-redux';
 import walletService from '../services/walletService';
 import utils, { TX0_MIN_CONFIRMATIONS } from '../services/utils';
+import Tx0Modal from '../components/Tx0Modal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as Icons from '@fortawesome/free-solid-svg-icons';
 import mixService from '../services/mixService';
-import backendService from '../services/backendService';
 
 class DepositPage extends Component {
 
@@ -31,9 +30,6 @@ class DepositPage extends Component {
 
     this.handleShowTx0 = this.handleShowTx0.bind(this);
     this.handleCloseTx0 = this.handleCloseTx0.bind(this);
-    this.handleChangePoolTx0 = this.handleChangePoolTx0.bind(this);
-    this.handleChangeMixsTargetTx0 = this.handleChangeMixsTargetTx0.bind(this);
-    this.handleSubmitTx0 = this.handleSubmitTx0.bind(this)
   }
 
   handleClose() {
@@ -69,53 +65,16 @@ class DepositPage extends Component {
   }
 
   handleShowTx0(utxo) {
-    const setState = poolsResponse => {
-      console.log('poolsResponse',poolsResponse)
-      this.setState({
-        showTx0: {
-          utxo: utxo,
-          poolsResponse: poolsResponse,
-          poolId: poolsResponse.pools.length>0 ? poolsResponse.pools[0].poolId : undefined,
-          mixsTarget: 0
-        }
-      })
-    }
-    return backendService.tx0.fetchPools(utxo.value).then(setState)
-  }
-
-  handleChangePoolTx0(e) {
-    const poolId = e.target.value
-
-    const showTx0New = Object.assign({}, this.state.showTx0)
-    showTx0New.poolId = poolId
     this.setState({
-      showTx0: showTx0New
+      showTx0: utxo
     })
-  }
-
-  handleChangeMixsTargetTx0(e) {
-    const mixsTarget = parseInt(e.target.value)
-
-    const showTx0New = Object.assign({}, this.state.showTx0)
-    showTx0New.mixsTarget = mixsTarget
-    this.setState({
-      mixsTarget: mixsTarget
-    })
-  }
-
-  handleSubmitTx0() {
-    backendService.tx0.create(this.state.showTx0.utxo.hash, this.state.showTx0.utxo.index, this.state.showTx0.poolId, this.state.showTx0.mixsTarget).then((tx0Response) => {
-      console.log('tx0Response',tx0Response)
-      walletService.fetchState()
-    })
-    this.handleCloseTx0();
   }
 
   render() {
     const utxosDeposit = walletService.getUtxosDeposit()
     return (
       <div className='depositPage'>
-        <Modal show={this.state.show} onHide={this.handleClose} animation={false}>
+        <Modal show={this.state.show} onHide={this.handleClose}>
             <Modal.Header>
             <Modal.Title>Start mixing</Modal.Title>
           </Modal.Header>
@@ -133,36 +92,7 @@ class DepositPage extends Component {
           </Modal.Footer>
         </Modal>
 
-        {this.state.showTx0 && <Modal show={this.state.showTx0} onHide={this.handleCloseTx0} animation={false} dialogClassName="modal-tx0">
-          <Modal.Header>
-            <Modal.Title>Add to premix<br/>
-              <small>{this.state.showTx0.utxo.hash}:{this.state.showTx0.utxo.index}</small></Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <p>
-              This will split your UTXO and start mixing.<br/>
-              Value: <strong>{utils.toBtc(this.state.showTx0.utxo.value)}btc</strong><br/>
-              <br/>
-              Select a pool:
-              <select className="form-control" onChange={this.handleChangePoolTx0} defaultValue={this.state.showTx0.poolId}>
-                {this.state.showTx0.poolsResponse.pools.map(pool => <option key={pool.poolId} value={pool.poolId}>{pool.poolId} (denomination: {utils.toBtc(pool.denomination)}btc, anonymity set: {pool.mixAnonymitySet}, connected: {pool.nbRegistered}, last mix: {moment.duration(pool.elapsedTime).humanize()})</option>)}
-              </select><br/>
-
-              Mixs target: <select className="form-control col-sm-2" onChange={this.handleChangeMixsTargetTx0} defaultValue={this.state.showTx0.mixsTarget}>
-              <option value={1}>1</option>
-              <option value={2}>2</option>
-              <option value={3}>3</option>
-              <option value={5}>5</option>
-              <option value={10}>10</option>
-              <option value={0}>∞</option>
-            </select> (you can change this later)
-            </p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.handleCloseTx0}>Close</Button>
-            <Button onClick={this.handleSubmitTx0}>Start mixing</Button>
-          </Modal.Footer>
-        </Modal>}
+        {this.state.showTx0 && <Tx0Modal utxo={this.state.showTx0} onClose={this.handleCloseTx0}/>}
 
         <div className='row'>
           <div className='col-sm-2'>
@@ -209,166 +139,6 @@ class DepositPage extends Component {
               </td>
             </tr>
           })}
-          {false && <todo>
-            <tr>
-              <th scope="row">1</th>
-              <td>
-                <small>9353e3c299b84fc3...02e0c:8</small>
-              </td>
-              <td>0.28</td>
-              <td>0.01btc</td>
-              <td><span className='text-primary'>TX0</span></td>
-              <td>
-                <div className="progress">
-                  <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar"
-                       aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style={{ width: '100%' }}>
-                    TX0&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                  </div>
-                </div>
-              </td>
-              <td>5s ago</td>
-              <td>
-                <button className='btn btn-sm btn-primary' title='Stop'><Icon.Square size={12}/></button>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>
-                <small>198787d1085b1d94...c8476:1</small>
-              </td>
-              <td>0.64</td>
-              <td>-</td>
-              <td>READY</td>
-              <td>
-
-              </td>
-              <td>26d ago</td>
-              <td>
-                <button className='btn btn-sm btn-primary' title='TX0' onClick={this.handleShowTx0}><Icon.Plus size={12}/>
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>
-                <small>198787d1085b1d94...c8476:1</small>
-              </td>
-              <td>0.014</td>
-              <td>-</td>
-              <td>READY</td>
-              <td>
-
-              </td>
-              <td>31d ago</td>
-              <td>
-                <button className='btn btn-sm btn-primary' title='TX0' onClick={this.handleShowTx0}><Icon.Plus size={12}/>
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">4</th>
-              <td>
-                <small>198787d1085b1d94...c8476:1</small>
-              </td>
-              <td>0.1241</td>
-              <td>-</td>
-              <td>READY</td>
-              <td>
-
-              </td>
-              <td>41d ago</td>
-              <td>
-                <button className='btn btn-sm btn-primary' title='TX0' onClick={this.handleShowTx0}><Icon.Plus size={12}/>
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>
-                <small>198787d1085b1d94...c8476:1</small>
-              </td>
-              <td>0.0014</td>
-              <td>-</td>
-              <td>READY</td>
-              <td>
-
-              </td>
-              <td>73d ago</td>
-              <td>
-                <button className='btn btn-sm btn-primary' title='TX0' onClick={this.handleShowTx0}><Icon.Plus size={12}/>
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">5</th>
-              <td>
-                <small>198787d1085b1d94...c8476:1</small>
-              </td>
-              <td>0.1083</td>
-              <td>-</td>
-              <td>READY</td>
-              <td>
-
-              </td>
-              <td>82d ago</td>
-              <td>
-                <button className='btn btn-sm btn-primary' title='TX0' onClick={this.handleShowTx0}><Icon.Plus size={12}/>
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">6</th>
-              <td>
-                <small>198787d1085b1d94...c8476:1</small>
-              </td>
-              <td>0.31</td>
-              <td>-</td>
-              <td>READY</td>
-              <td>
-
-              </td>
-              <td>26d ago</td>
-              <td>
-                <button className='btn btn-sm btn-primary' title='TX0' onClick={this.handleShowTx0}><Icon.Plus size={12}/>
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">7</th>
-              <td>
-                <small>198787d1085b1d94...c8476:1</small>
-              </td>
-              <td>1.2</td>
-              <td>-</td>
-              <td>READY</td>
-              <td>
-
-              </td>
-              <td>128d ago</td>
-              <td>
-                <button className='btn btn-sm btn-primary' title='TX0' onClick={this.handleShowTx0}><Icon.Plus size={12}/>
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">8</th>
-              <td>
-                <small>198787d1085b1d94...c8476:1</small>
-              </td>
-              <td>0.04</td>
-              <td>-</td>
-              <td>READY</td>
-              <td>
-
-              </td>
-              <td>2 years ago</td>
-              <td>
-                <button className='btn btn-sm btn-primary' title='TX0' onClick={this.handleShowTx0}><Icon.Plus size={12}/>
-                </button>
-              </td>
-            </tr>
-          </todo>
-          }
           </tbody>
         </table>
         </div>
