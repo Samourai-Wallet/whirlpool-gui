283bfb7afa4c25449e559bfe079ccaeaefa3cbbc
zeroleak <zeroleak@samourai.io>
2019-02-22 21:14:51 +0100

    add abstractModal

--- app/components/Modals/DepositModal.js
+++ app/components/Modals/DepositModal.js
@@ -1,21 +1,18 @@
 // @flow
 import React from 'react';
-import { Alert, Button, Modal } from 'react-bootstrap';
 import walletService from '../../services/walletService';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import QRCode from 'qrcode.react';
 import * as Icons from '@fortawesome/free-solid-svg-icons';
+import AbstractModal from './AbstractModal';
+import { Button } from 'react-bootstrap';
 
-export default class DepositPage extends React.PureComponent {
+export default class DepositPage extends AbstractModal {
   constructor(props) {
-    super(props)
-
-    this.state = {
-      loaded: false,
-      error: undefined,
-
+    const initialState = {
       depositAddress: undefined
-    };
+    }
+    super(props, 'modal-deposit', initialState)
 
     this.handleNextDepositAddress = this.handleNextDepositAddress.bind(this)
 
@@ -26,14 +23,13 @@ export default class DepositPage extends React.PureComponent {
   doFetchDepositAddressAndShow(distinct=false) {
     const setState = depositAddressResponse => {
       this.setState({
-        loaded: true,
         depositAddress: depositAddressResponse
       })
     }
     if (distinct) {
-      return walletService.fetchDepositAddressDistinct(this.state.depositAddress).then(setState)
+      this.loading("Fetching another deposit address...", walletService.fetchDepositAddressDistinct(this.state.depositAddress).then(setState))
     } else {
-      return walletService.fetchDepositAddress().then(setState)
+      this.loading("Fetching deposit address...", walletService.fetchDepositAddress().then(setState))
     }
   }
 
@@ -41,33 +37,27 @@ export default class DepositPage extends React.PureComponent {
     this.doFetchDepositAddressAndShow(true)
   }
 
-  render() {
-    return (
-      <Modal show={true} onHide={this.props.onClose} dialogClassName="modal-deposit">
-        <Modal.Header>
-          <Modal.Title>Deposit</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {this.state.loaded && this.state.error && (
-            <Alert variant='danger'>{this.state.error}</Alert>
-          )}
-          {this.state.loaded && !this.state.error && (
-            <div>
-              Deposit funds to your deposit wallet:
-              <div className='depositAddress'>
-                <QRCode value={this.state.depositAddress} /><br/>
-                <b>{this.state.depositAddress}</b> <a onClick={this.handleNextDepositAddress} title='Change deposit address'><FontAwesomeIcon icon={Icons.faRedo} /></a>
-              </div>
-            </div>
-          )}
-        </Modal.Body>
-        <Modal.Footer>
-          {!this.state.loaded && <div className="modal-status">
-            <div className="spinner-border spinner-border-sm" role="status"/> Fetching deposit address...
-          </div>}
-          <Button variant="secondary" onClick={this.props.onClose}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    );
+  renderTitle() {
+    return <span>Deposit</span>
+  }
+
+  renderButtons() {
+    return <div>
+      {!this.isLoading() && <Button onClick={this.handleNextDepositAddress}>Generate another address <FontAwesomeIcon icon={Icons.faRedo} /></Button>}
+    </div>
+  }
+
+  renderBody() {
+    return <div>
+      {!this.isLoading() && !this.isError() && (
+        <div>
+          Deposit funds to your deposit wallet:
+          <div className='depositAddress'>
+            <QRCode value={this.state.depositAddress} /><br/>
+            <b>{this.state.depositAddress}</b>
+          </div>
+        </div>
+      )}
+    </div>
   }
 }
--- app/components/Modals/Tx0Modal.js
+++ app/components/Modals/Tx0Modal.js
@@ -1,47 +1,37 @@
 // @flow
-import React, { Component } from 'react';
-import { Button, Modal, Alert } from 'react-bootstrap';
+import React from 'react';
+import { Button } from 'react-bootstrap';
 import moment from 'moment';
-import walletService from '../../services/walletService';
 import utils from '../../services/utils';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import backendService from '../../services/backendService';
+import mixService from '../../services/mixService';
+import AbstractModal from './AbstractModal';
 
-export default class DepositPage extends React.PureComponent {
+export default class Tx0Modal extends AbstractModal {
   constructor(props) {
-    super(props)
-
-    this.state = {
-      loaded: false,
-      error: undefined,
-
+    const initialState = {
       pools: undefined,
       poolId: undefined,
       mixsTarget: 0
-    };
+    }
+    super(props, 'modal-tx0', initialState)
 
     // fetch pools for tx0
-    backendService.tx0.fetchPools(props.utxo.value).then(poolsResponse => {
-      console.log('poolsResponse', poolsResponse)
+    this.loading("Fetching pools for tx0...", mixService.pools(props.utxo).then(poolsResponse => {
       const pools = poolsResponse.pools
 
-      let error = undefined
       if (pools.length == 0) {
-        error = "No pool found for this utxo value."
+        this.setError("No pool found for this utxo.")
       }
       this.setState({
         pools: pools,
         poolId: pools.length > 0 ? pools[0].poolId : undefined,
-        mixsTarget: 0,
-        error: error,
-        loaded: true
+        mixsTarget: 0
       })
-    })
+    }))
 
     this.handleChangePoolTx0 = this.handleChangePoolTx0.bind(this);
     this.handleChangeMixsTargetTx0 = this.handleChangeMixsTargetTx0.bind(this);
     this.handleSubmitTx0 = this.handleSubmitTx0.bind(this)
-
   }
 
   handleChangePoolTx0(e) {
@@ -61,55 +51,42 @@ export default class DepositPage extends React.PureComponent {
   }
 
   handleSubmitTx0() {
-    backendService.tx0.create(this.props.utxo.hash, this.props.utxo.index, this.state.poolId, this.state.mixsTarget).then((tx0Response) => {
-      console.log('tx0Response',tx0Response)
-      walletService.fetchState()
-    })
+    mixService.tx0(this.props.utxo, this.state.poolId, this.state.mixsTarget)
     this.props.onClose();
   }
 
-  render() {
-    return (
-      <Modal show={true} onHide={this.props.onClose} dialogClassName="modal-tx0">
-        <Modal.Header>
-          <Modal.Title>Add to premix<br/>
-            <small>{this.props.utxo.hash}:{this.props.utxo.index}</small></Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            This will split your UTXO and start mixing.<br/>
-            Value: <strong>{utils.toBtc(this.props.utxo.value)}btc</strong><br/>
-            <br/>
+  renderTitle() {
+    return <div>
+      Add to premix<br/>
+      <small>{this.props.utxo.hash}:{this.props.utxo.index}</small>
+    </div>
+  }
 
-            {this.state.loaded && this.state.error && (
-              <Alert variant='danger'>{this.state.error}</Alert>
-            )}
-            {this.state.loaded && !this.state.error && (
-              <div>
-                Select a pool:
-                <select className="form-control" onChange={this.handleChangePoolTx0} defaultValue={this.state.poolId}>
-                  {this.state.pools.map(pool => <option key={pool.poolId} value={pool.poolId}>{pool.poolId} (denomination: {utils.toBtc(pool.denomination)}btc, anonymity set: {pool.mixAnonymitySet}, connected: {pool.nbRegistered}, last mix: {moment.duration(pool.elapsedTime).humanize()})</option>)}
-                </select><br/>
+  renderButtons() {
+    return <Button onClick={this.handleSubmitTx0}>Start mixing</Button>
+  }
 
-                Mixs target:
-                <select className="form-control col-sm-2" onChange={this.handleChangeMixsTargetTx0} defaultValue={this.state.mixsTarget}>
-                  <option value={1}>1</option>
-                  <option value={2}>2</option>
-                  <option value={3}>3</option>
-                  <option value={5}>5</option>
-                  <option value={10}>10</option>
-                  <option value={0}>∞</option>
-                </select> (you can change this later)
-              </div>
-            )}
-        </Modal.Body>
-        <Modal.Footer>
-          {!this.state.loaded && <div className="modal-status">
-            <div className="spinner-border spinner-border-sm" role="status"/> Fetching pools for tx0...
-          </div>}
-          <Button variant="secondary" onClick={this.props.onClose}>Close</Button>
-          {this.state.loaded && !this.state.error && <Button onClick={this.handleSubmitTx0}>Start mixing</Button>}
-        </Modal.Footer>
-      </Modal>
-    );
+  renderBody() {
+    return <div>
+      This will split your UTXO and start mixing.<br/>
+      Value: <strong>{utils.toBtc(this.props.utxo.value)}btc</strong><br/>
+      <br/>
+      {!this.isLoading() && !this.isError() && <div>
+        Select a pool:
+        <select className="form-control" onChange={this.handleChangePoolTx0} defaultValue={this.state.poolId}>
+          {this.state.pools.map(pool => <option key={pool.poolId} value={pool.poolId}>{pool.poolId} (denomination: {utils.toBtc(pool.denomination)}btc, anonymity set: {pool.mixAnonymitySet}, connected: {pool.nbRegistered}, last mix: {moment.duration(pool.elapsedTime).humanize()})</option>)}
+        </select><br/>
+
+        Mixs target:
+        <select className="form-control col-sm-2" onChange={this.handleChangeMixsTargetTx0} defaultValue={this.state.mixsTarget}>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={5}>5</option>
+          <option value={10}>10</option>
+          <option value={0}>∞</option>
+        </select> (you can change this later)
+      </div>}
+    </div>
   }
 }
