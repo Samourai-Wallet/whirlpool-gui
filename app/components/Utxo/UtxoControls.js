fcaaef730e7a28dfd96d61c78fad8d7c412750c5
zeroleak <zeroleak@samourai.io>
2019-03-06 15:27:59 +0100

    fix start/stop utxo button

--- app/components/Utxo/UtxoControls.js
+++ app/components/Utxo/UtxoControls.js
@@ -18,8 +18,8 @@ class UtxoControls extends React.PureComponent {
       <div>
         {utxo.account == WHIRLPOOL_ACCOUNTS.DEPOSIT && utxo.confirmations === 0 && <small>unconfirmed</small>}
         {utxo.account == WHIRLPOOL_ACCOUNTS.DEPOSIT && mixService.isTx0Possible(utxo) && <button className='btn btn-sm btn-primary' title='Tx0' onClick={() => mixService.tx0(utxo)} >Tx0 <Icon.ChevronsRight size={12}/></button>}
-        {mixService.isStartMixPossible(utxo) && <button className='btn btn-sm btn-primary' title='Start mixing' onClick={() => mixService.startMix(utxo)}>Mix <Icon.Play size={12} /></button>}
-        {mixService.isStopMixPossible(utxo) && <button className='btn btn-sm btn-primary' title='Stop mixing' onClick={() => mixService.stopMix(utxo)}>Stop <Icon.Square size={12} /></button>}
+        {mixService.isStartMixPossible(utxo) && <button className='btn btn-sm btn-primary' title='Start mixing' onClick={() => mixService.startMixUtxo(utxo)}>Mix <Icon.Play size={12} /></button>}
+        {mixService.isStopMixPossible(utxo) && <button className='btn btn-sm btn-primary' title='Stop mixing' onClick={() => mixService.stopMixUtxo(utxo)}>Stop <Icon.Square size={12} /></button>}
       </div>
     )
   }
