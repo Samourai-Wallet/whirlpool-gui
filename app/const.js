b58daf3bec1fc03294ca02237fa93a3ee4d6c2b8
zeroleak <zeroleak@samourai.io>
2019-03-29 16:53:16 +0100

    prepare for 0.0.5

--- app/const.js
+++ app/const.js
@@ -3,33 +3,34 @@ import { computeLogPath, logger } from './utils/logger';
 
 /* shared with mainProcess */
 
-export const CLI_FILENAME = "whirlpool-client-cli-0.0.4-run.jar";
-export const CLI_URL = "https://github.com/Samourai-Wallet/whirlpool-runtimes/releases/download/cli-0.0.4/whirlpool-client-cli-0.0.4-run.jar";
-export const CLI_CHECKSUM = "2a8a3aba69721d2b0fbbb0c103639cb5";
+export const CLI_FILENAME = 'whirlpool-client-cli-0.0.5-run.jar';
+export const CLI_URL =
+  'https://github.com/Samourai-Wallet/whirlpool-runtimes/releases/download/cli-0.0.5/whirlpool-client-cli-0.0.5-run.jar';
+export const CLI_CHECKSUM = 'c917255907f432221c4d2624bd415e3a';
 
-export const DEFAULT_CLI_LOCAL = true
-export const DEFAULT_CLIPORT = 8899
+export const DEFAULT_CLI_LOCAL = true;
+export const DEFAULT_CLIPORT = 8899;
 
 export const IPC_CLILOCAL = {
   GET_STATE: 'cliLocal.getState',
   STATE: 'cliLocal.state',
   RELOAD: 'cliLocal.reload'
-}
+};
 export const CLILOCAL_STATUS = {
   DOWNLOADING: 'DOWNLOADING',
   ERROR: 'ERROR',
   READY: 'READY'
-}
+};
 export const WHIRLPOOL_SERVER = {
   TEST: 'Whirlpool TESTNET',
   MAIN: 'Whirlpool MAINNET'
-}
+};
 
-export const STORE_CLILOCAL = "cli.local"
+export const STORE_CLILOCAL = 'cli.local';
 
-const app = (electron.app || electron.remote.app)
+const app = electron.app || electron.remote.app;
 
-export const DL_PATH = app.getPath('userData')
+export const DL_PATH = app.getPath('userData');
 
-export const CLI_LOG_FILE = computeLogPath('whirlpool-cli.log')
-export const GUI_LOG_FILE = logger.getFile()
+export const CLI_LOG_FILE = computeLogPath('whirlpool-cli.log');
+export const GUI_LOG_FILE = logger.getFile();
